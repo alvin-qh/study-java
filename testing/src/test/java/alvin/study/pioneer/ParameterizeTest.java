95fc83d3bfa3ab6586cc837b8c03cc9dfda448e9
Alvin <quhao317@163.com>
2023-01-16 01:33:49 +0800

    [testing] add junit pioneer libaray demo

---
+++ testing/src/test/java/alvin/study/pioneer/ParameterizeTest.java
@@ -0,0 +1,380 @@
+package alvin.study.pioneer;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.support.AnnotationConsumer;
+import org.junitpioneer.jupiter.cartesian.ArgumentSets;
+import org.junitpioneer.jupiter.cartesian.CartesianArgumentsSource;
+import org.junitpioneer.jupiter.cartesian.CartesianMethodArgumentsProvider;
+import org.junitpioneer.jupiter.cartesian.CartesianParameterArgumentsProvider;
+import org.junitpioneer.jupiter.cartesian.CartesianTest;
+import org.junitpioneer.jupiter.cartesian.CartesianTest.Enum;
+import org.junitpioneer.jupiter.cartesian.CartesianTest.MethodFactory;
+import org.junitpioneer.jupiter.cartesian.CartesianTest.Values;
+import org.junitpioneer.jupiter.params.IntRangeSource;
+
+/**
+ * 该注解用于测试参数, 指定该参数值的生成规则
+ *
+ * <p>
+ * {@link CartesianArgumentsSource @CartesianArgumentsSource} 注解用于指定实际生成参数值的类, 本例中通过
+ * {@link NumberSourceProvider} 类来实际产生参数值
+ * </p>
+ *
+ * <p>
+ * 参考 {@link ParameterizeTest#cartesian_shouldGenerateArgumentsByAnnotation(int)} 测试方法
+ * </p>
+ */
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@CartesianArgumentsSource(NumberSourceProvider.class)
+@interface NumberSource {
+    /**
+     * 参数值集合, 每次从该集合中产生一个参数值
+     *
+     * @return 参数值集合
+     */
+    int[] value();
+}
+
+/**
+ * 为 {@link NumberSource @NumberSource} 注解实际产生参数值的类
+ *
+ * <p>
+ * 该类型需要实现 {@link CartesianParameterArgumentsProvider} 接口, 其泛型参数指定了要生成参数的类型, 通过实现
+ * {@link CartesianParameterArgumentsProvider#provideArguments(ExtensionContext, Parameter)} 方法来产生参数集合
+ * </p>
+ *
+ * <p>
+ * 参考 {@link ParameterizeTest#cartesian_shouldGenerateArgumentsByAnnotation(int)} 测试方法
+ * </p>
+ */
+class NumberSourceProvider implements CartesianParameterArgumentsProvider<Integer> {
+    /**
+     * 为 {@link NumberSource @NumberSource} 注解实际产生参数值集合
+     *
+     * @param context   测试方法的上下文对象, 包括测试方法的各类信息
+     * @param parameter 被 {@link NumberSource @NumberSource} 注解的参数对象
+     * @return 生成的参数值集合的 {@link Stream} 对象
+     */
+    @Override
+    public Stream<Integer> provideArguments(ExtensionContext context, Parameter parameter) throws Exception {
+        // 从参数对象上获取注解对象
+        var anno = Objects.requireNonNull(
+            parameter.getAnnotation(NumberSource.class));
+
+        // 从注解中获取生成参数的规则, 并生成参数值集合
+        // 注意, 集合元素不能为值类型, 这里使用了 boxed 方法将值类型转为引用类型
+        return Arrays.stream(Objects.requireNonNull(anno.value())).boxed();
+    }
+}
+
+/**
+ * 该注解用于测试方法, 指定该方法所有参数值的生成规则
+ *
+ * <p>
+ * {@link CartesianArgumentsSource @CartesianArgumentsSource} 注解用于指定实际生成参数值的类, 本例中通过
+ * {@link NumberArgumentSourceProvider} 类来实际产生参数值
+ * </p>
+ *
+ * <p>
+ * 为了简单起见, 本例只使用一种参数产生规则 {@link NumberArgumentSource#value()}, 实际使用时, 可以定义任意属性来产生所需的参数值
+ * </p>
+ *
+ * <p>
+ * 参考 {@link ParameterizeTest#cartesian_shouldGenerateAllArgumentsByAnnotation(int, int)} 测试方法
+ * </p>
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@CartesianArgumentsSource(NumberArgumentSourceProvider.class)
+@interface NumberArgumentSource {
+    /**
+     * 参数值集合, 每次从该集合中产生一个参数值
+     *
+     * @return 参数值集合
+     */
+    int[] value();
+}
+
+/**
+ * 为 {@link NumberArgumentSource @NumberArgumentSource} 注解的方法所有参数实际产生参数值的类
+ *
+ * <p>
+ * 该类型需要实现 {@link CartesianMethodArgumentsProvider} 接口, 通过实现
+ * {@link CartesianMethodArgumentsProvider#provideArguments(ExtensionContext)} 方法来为测试方法的所有参数产生参数值
+ * </p>
+ *
+ * <p>
+ * 为了简单起见, 本例为所有的参数都按同一种规则产生参数值 ({@link NumberArgumentSource#value()}), 实际使用时可以产生任何类型参数的组合
+ * </p>
+ *
+ * <p>
+ * 为了能获取到注解对象, 本例实现了 {@link AnnotationConsumer} 接口, 其 {@link AnnotationConsumer#accept(Object)} 方法将测试
+ * 方法上的 {@link NumberArgumentSource @NumberArgumentSource} 注解对象作为参数传递
+ * </p>
+ *
+ * <p>
+ * 参考 {@link ParameterizeTest#cartesian_shouldGenerateArgumentsByAnnotation(int)} 测试方法
+ * </p>
+ */
+class NumberArgumentSourceProvider implements CartesianMethodArgumentsProvider,
+                                   AnnotationConsumer<NumberArgumentSource> {
+    // NumberArgumentSource 注解的 value 属性值, 作为产生参数值的规则
+    private int[] numbers;
+
+    /**
+     * 传递注解对象
+     *
+     * @param source 定义在测试方法上的 {@link NumberArgumentSource @NumberArgumentSource} 注解对象
+     */
+    @Override
+    public void accept(NumberArgumentSource source) {
+        // 保持注解 value 属性值
+        this.numbers = Objects.requireNonNull(source.value());
+    }
+
+    /**
+     * 为 {@link NumberArgumentSource @NumberArgumentSource} 注解的测试方法实际产生参数值集合
+     *
+     * @param context 测试方法的上下文对象, 包括测试方法的各类信息
+     * @return 生成的参数值集合的 {@link ArgumentSets} 对象, 包括测试方法的所有参数值
+     */
+    @Override
+    public ArgumentSets provideArguments(ExtensionContext context) throws Exception {
+        // 创建保持参数集合
+        var argSets = ArgumentSets.create();
+
+        // 根据目标测试方法的参数数量, 生成对应的测试参数值
+        // 根据 numbers 的设置, 为每个参数生成对应的一组参数值
+        for (var i = 0; i < context.getRequiredTestMethod().getParameterCount(); i++) {
+            argSets.argumentsForNextParameter(Arrays.stream(numbers).boxed());
+        }
+        return argSets;
+    }
+
+}
+
+/**
+ * 测试 pioneer 库的参数化功能
+ */
+class ParameterizeTest {
+    /**
+     * 以笛卡尔积将设定的参数进行组合传递
+     *
+     * <p>
+     * 通过 {@link Values @Values} 注解设定的两个参数 {@code x} 和 {@code y} 会将以 {@code (x=1, y=3)}, {@code (x=1, y=4)},
+     * {@code (x=2, y=3)} 以及 {@code (x=2, y=4)} 的组合进行参数传递, 共执行 4 此测试
+     * </p>
+     *
+     * @param x 第一组参数
+     * @param y 第二组参数
+     */
+    @CartesianTest
+    void cartesian_shouldInjectCartesianArguments(
+            @Values(ints = { 1, 2 }) int x,
+            @Values(ints = { 3, 4 }) int y) {
+        var expected = new Object[] {
+            new int[] { 1, 3 },
+            new int[] { 1, 4 },
+            new int[] { 2, 3 },
+            new int[] { 2, 4 }
+        };
+
+        then(new int[] { x, y }).isIn(expected);
+    }
+
+    enum TestEnum {
+        FIRST, SECOND;
+    }
+
+    /**
+     * 设置参数的取值范围
+     *
+     * <p>
+     * {@link IntRangeSource @IntRangeSource} 注解用于设置一系列在指定范围内的参数, 类似功能的注解还包括
+     * {@link org.junitpioneer.jupiter.params.ByteRangeSource @ByteRangeSource},
+     * {@link org.junitpioneer.jupiter.params.ShortRangeSource @ShortRangeSource},
+     * {@link org.junitpioneer.jupiter.params.LongRangeSource @LongRangeSource},
+     * {@link org.junitpioneer.jupiter.params.FloatRangeSource @FloatRangeSource} 以及
+     * {@link org.junitpioneer.jupiter.params.DoubleRangeSource @DoubleRangeSource} 注解
+     * </p>
+     *
+     * <p>
+     * 另外, 可以通过
+     * {@link org.junitpioneer.jupiter.params.IntRangeSource.IntRangeSources @IntRangeSources},
+     * {@link org.junitpioneer.jupiter.params.ByteRangeSource.ByteRangeSources @ByteRangeSources},
+     * {@link org.junitpioneer.jupiter.params.ShortRangeSource.ShortRangeSources @ShortRangeSources},
+     * {@link org.junitpioneer.jupiter.params.LongRangeSource.LongRangeSources @LongRangeSources},
+     * {@link org.junitpioneer.jupiter.params.FloatRangeSource.FloatRangeSources @FloatRangeSources} 以及
+     * {@link org.junitpioneer.jupiter.params.DoubleRangeSource.DoubleRangeSources @DoubleRangeSources} 注解组合
+     * 多个范围设置, 进行批量设置
+     * </p>
+     *
+     * <p>
+     * {@link Enum @Enum} 注解的作用是遍历一个枚举的所有值, 其 {@code value} 属性表示要遍历的枚举的类型, 当枚举类型和参数类型一致时
+     * 可以省略 (例如本例可以省略), 否则必须显式声明, 例如: {@code @Enum(ChronoUnit.class) TemporalUnit unit} 参数
+     * </p>
+     *
+     * <p>
+     * {@link Enum @Enum} 注解具备 {@link names} 属性, 默认情况表示要包含的枚举名称, 例如:
+     * <code>@Enum(names = { "DAYS", "HOURS" }) ChronoUnit unit</code> 参数, 表示只包含 {@code DAYS} 和 {@code HOURS} 两项
+     * </p>
+     *
+     * <p>
+     * {@link Enum @Enum} 注解具备 {@link mode} 属性, 取值为 {@link Enum.Mode} 枚举, 表示 {@code names} 属性的含义, 默认为
+     * {@link Enum.Mode#INCLUDE}, 表达如下:
+     * <ul>
+     * <li>
+     * {@link Enum.Mode#INCLUDE}: 表示 {@code names} 属性指定的为要要包含的项
+     * </li>
+     * <li>
+     * {@link Enum.Mode#EXCLUDE}: 表示 {@code names} 属性指定的为要要排除的项
+     * </li>
+     * <li>
+     * {@link Enum.Mode#MATCH_ALL}: 表示 {@code names} 为一组正则表达式, 取匹配所有正则表达式的枚举项
+     * </li>
+     * <li>
+     * {@link Enum.Mode#MATCH_ANY}: 表示 {@code names} 为一组正则表达式, 取匹配任一正则表达式的枚举项
+     * </li>
+     * </ul>
+     * </p>
+     *
+     * @param x 第一组参数, 为 {@code [1..3)} 范围内的所有整数
+     * @param s 第二组参数, 为 <code>{"A", "B"}</code> 范围内的所有字符串
+     * @param e 第二组参数, 为 {@link TestEnum} 枚举包含的所有值
+     */
+    @CartesianTest
+    void cartesian_shouldPassArgumentsInRange(
+            @IntRangeSource(from = 1, to = 3, step = 1) int x,
+            @Values(strings = { "A", "B" }) String s,
+            @Enum(TestEnum.class) TestEnum e) {
+        var expected = new Object[] {
+            new Object[] { 1, "A", TestEnum.FIRST },
+            new Object[] { 1, "A", TestEnum.SECOND },
+            new Object[] { 1, "B", TestEnum.FIRST },
+            new Object[] { 1, "B", TestEnum.SECOND },
+            new Object[] { 2, "A", TestEnum.FIRST },
+            new Object[] { 2, "A", TestEnum.SECOND },
+            new Object[] { 2, "B", TestEnum.FIRST },
+            new Object[] { 2, "B", TestEnum.SECOND }
+        };
+
+        then(new Object[] { x, s, e }).isIn(expected);
+    }
+
+    /**
+     * 为 {@link #cartesian_shouldGenerateArgumentsByFactoryMethod(int, String, TestEnum)} 测试方法提供测试参数
+     *
+     * <p>
+     * 注意, 通过 {@link IntStream#range(int, int)} 得到的结果无法作为参数传递, 需要通过 {@link IntStream#boxed()} 方法转为
+     * {@code Stream<Integer>} 类型
+     * </p>
+     *
+     * <p>
+     * 本方法需要声明为 {@code static} 静态方法, 如果要设置为非静态, 则测试类上必须标记
+     * {@link org.junit.jupiter.api.TestInstance @TestInstance} 注解, 且
+     * {@code value} 属性必须为 {@link org.junit.jupiter.api.TestInstance.Lifecycle#PER_CLASS Lifecycle.PER_CLASS}, 即:
+     * {@code @TestInstance(Lifecycle#PER_CLASS)}
+     * </p>
+     *
+     * <p>
+     * 关于 {@code PER_CLASS} 生命周期的说明, 请参见 {@link alvin.study.junit.LifecycleTest LifecycleTest} 范例
+     * </p>
+     *
+     * @return {@link ArgumentSets} 参数集合对象
+     */
+    static ArgumentSets argumentsGenerator() {
+        return ArgumentSets
+                .argumentsForFirstParameter(IntStream.range(1, 3).boxed())
+                .argumentsForNextParameter("A", "B")
+                .argumentsForNextParameter(TestEnum.values());
+    }
+
+    /**
+     * 通过 {@link #argumentsGenerator()} 方法的返回值作为本测试的参数输入
+     *
+     * @param x 第一组参数, 为 {@code [1..3)} 范围内的所有整数
+     * @param s 第二组参数, 为 <code>{"A", "B"}</code> 范围内的所有字符串
+     * @param e 第二组参数, 为 {@link TestEnum} 枚举包含的所有值
+     */
+    @CartesianTest
+    @MethodFactory("argumentsGenerator")
+    void cartesian_shouldGenerateArgumentsByFactoryMethod(int x, String s, TestEnum e) {
+        var expected = new Object[] {
+            new Object[] { 1, "A", TestEnum.FIRST },
+            new Object[] { 1, "A", TestEnum.SECOND },
+            new Object[] { 1, "B", TestEnum.FIRST },
+            new Object[] { 1, "B", TestEnum.SECOND },
+            new Object[] { 2, "A", TestEnum.FIRST },
+            new Object[] { 2, "A", TestEnum.SECOND },
+            new Object[] { 2, "B", TestEnum.FIRST },
+            new Object[] { 2, "B", TestEnum.SECOND }
+        };
+
+        then(new Object[] { x, s, e }).isIn(expected);
+    }
+
+    /**
+     * 通过 {@link NumberSource @NumberSource} 注解参数以产生测试参数值
+     *
+     * <p>
+     * 测试参数的产生参考 {@link NumberSourceProvider#provideArguments(ExtensionContext, Parameter)} 方法
+     * </P>
+     *
+     * @param x 测试参数值
+     */
+    @CartesianTest
+    void cartesian_shouldGenerateArgumentsByAnnotation(
+            @NumberSource({ 1, 2, 3 }) int x) {
+        then(x).isIn(1, 2, 3);
+    }
+
+    /**
+     * 通过 {@link NumberArgumentSource @NumberArgumentSource} 注解方法, 为测试方法的每个参数产生一组测试值
+     *
+     * <p>
+     * 测试参数的产生参考 {@link NumberArgumentSourceProvider#provideArguments(ExtensionContext)} 方法
+     * </P>
+     *
+     * <p>
+     * 本例中为简单起见, 为每个参数生成值的规则是相同的, 所以两个参数都会产生 <code>{1, 2, 3}</code> 这三个值, 根据笛卡尔积的组合原则,
+     * 会产生 {@code (1, 1)}, {@code (1, 2)}, {@code (1, 3)}, {@code (2, 1)}, {@code (2, 2)}, {@code (2, 3)},
+     * {@code (3, 1)}, {@code (3, 2)} 和 {@code (3, 3)} 共 9 组参数值, 分别传递给 {@code x} 和 {@code y} 参数
+     * </p>
+     *
+     * @param x 测试参数值
+     * @param y 测试参数值
+     */
+    @CartesianTest
+    @NumberArgumentSource({ 1, 2, 3 })
+    void cartesian_shouldGenerateAllArgumentsByAnnotation(int x, int y) {
+        var expected = new Object[] {
+            new int[] { 1, 1 },
+            new int[] { 1, 2 },
+            new int[] { 1, 3 },
+            new int[] { 2, 1 },
+            new int[] { 2, 2 },
+            new int[] { 2, 3 },
+            new int[] { 3, 1 },
+            new int[] { 3, 2 },
+            new int[] { 3, 3 },
+        };
+
+        then(new int[] { x, y }).isIn(expected);
+    }
+}
