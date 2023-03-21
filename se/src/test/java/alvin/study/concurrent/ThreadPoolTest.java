cdd8a1842c307d41366d2ea75f0d2678da4bc077
Alvin <quhao317@163.com>
2023-03-21 18:47:03 +0800

    [se] add concurrent demo

--- se/src/test/java/alvin/study/concurrent/ThreadPoolTest.java
+++ se/src/test/java/alvin/study/concurrent/ThreadPoolTest.java
@@ -4,13 +4,16 @@ import static org.assertj.core.api.BDDAssertions.then;
 import static org.awaitility.Awaitility.await;
 
 import java.lang.ref.WeakReference;
+import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -73,6 +76,7 @@ import com.google.common.collect.Lists;
  * {@link FutureTask} 类型是 {@link Future} 接口的一个实现, 其同时也实现了 {@link Runnable} 接口
  * </p>
  */
+@SuppressWarnings("java:S2925")
 class ThreadPoolTest {
     // 保存线程执行器对象的集合, 用于在测试结束后进行关闭
     private WeakReference<ExecutorService> executorsHolder;
@@ -162,49 +166,167 @@ class ThreadPoolTest {
      */
     @Test
     void futureTask_shouldUseFutureTaskInThread() throws Exception {
+        // 定义 FutureTask 对象, 设置线程回调函数
         var task = new FutureTask<>(() -> Fibonacci.calculate(20));
 
+        // 实例化线程对象并启动
         var thread = new Thread(task);
         thread.start();
 
+        // 等待异步任务结束
         await().atMost(5, TimeUnit.SECONDS).until(task::isDone);
+
+        // 确认异步任务结束后, 线程也已经结束
         then(thread.isAlive()).isFalse();
+        // 确认计算结果
         then(task.get()).isEqualTo(6765);
     }
 
+    /**
+     * 通过线程池提交任务, 获取任务执行结果
+     *
+     * <p>
+     * 通过 {@link ExecutorService#submit(java.util.concurrent.Callable)
+     * ExecutorService.submit(Callable)} 方法可以向线程池提交一个任务, 返回一个 {@link FutureTask} 类型对象
+     * </p>
+     */
     @Test
     void futureTask_shouldCreateFutureTaskBySubmitThreadPool() throws Exception {
+        // 创建线程池执行器对象
         var executor = arrayBlockingQueueExecutor(20);
 
+        // 提交一个任务
         var task = executor.submit(() -> Fibonacci.calculate(20));
 
+        // 等待任务执行完毕
         await().atMost(5, TimeUnit.SECONDS).until(task::isDone);
+
+        // 确认任务结果
         then(task.get()).isEqualTo(6765);
     }
 
+    /**
+     * 提交多个任务
+     *
+     * <p>
+     * 本例中通过循环依次提交多个任务到线程池
+     * </p>
+     */
     @Test
-    void multiFutureTasks_() {
-        var results = Lists.<Future<Integer>>newArrayList();
+    void multiFutureTasks_shouldSubmitMultiTasks() {
+        // 保存 FutureTask 的集合对象
+        var tasks = Lists.<Future<Integer>>newArrayList();
+
+        // 任务计数器, 计算已完成任务数
         var resultCount = new AtomicInteger();
 
+        // 创建线程池执行器对象
         var executor = arrayBlockingQueueExecutor(20);
 
-        for (var i = 0; i < 20; i++) {
+        // 循环 20 次, 提交 20 个任务
+        for (var i = 1; i <= 20; i++) {
             var n = i;
 
-            results.add(executor.submit(() -> {
+            // 将任务提交返回的 FutureTask 对象保存到集合中
+            tasks.add(executor.submit(() -> {
                 try {
+                    // 执行计算
                     return Fibonacci.calculate(n + 1);
                 } finally {
+                    // 增加任务计数器
                     resultCount.incrementAndGet();
                 }
             }));
         }
 
+        // 等待任务全部结束
         await().atMost(5, TimeUnit.SECONDS).until(() -> resultCount.get() == 20);
 
-        then(results).allMatch(Future::isDone);
-        then(results).map(Future<Integer>::get).containsExactly(1, 1, 2, 3, 5, 8, 13, 21,
-            34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765);
+        // 确认所有的任务都执行完毕
+        then(tasks).allMatch(Future::isDone);
+
+        // 确认任务计算结果
+        then(tasks).map(Future<Integer>::get).containsExactly(1, 2, 3, 5, 8, 13, 21,
+            34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946);
+    }
+
+    /**
+     * 批量提交多个任务
+     *
+     * <p>
+     * 通过 {@link ExecutorService#invokeAll(java.util.Collection, long, TimeUnit)
+     * ExecutorService.invokeAll(Collection, long, TimeUnit)} 方法可以批量提交多个任务, 且返回每个任务 {@link FutureTask}
+     * 对象组成的集合
+     * </p>
+     */
+    @Test
+    void invokeAll_shouldSubmitMultiTasksAndInvokeThemAll() throws InterruptedException {
+        // 任务计数器, 计算已完成任务数
+        var resultCount = new AtomicInteger();
+
+        // 保存 FutureTask 的集合对象
+        var tasks = IntStream.range(1, 21)
+                .mapToObj(n -> {
+                    return (Callable<Integer>) () -> {
+                        try {
+                            // 执行计算
+                            return Fibonacci.calculate(n + 1);
+                        } finally {
+                            // 增加任务计数器
+                            resultCount.incrementAndGet();
+                        }
+                    };
+                })
+                .toList();
+
+        // 创建线程池执行器对象
+        var executor = arrayBlockingQueueExecutor(20);
+        // 执行所有任务
+        var futures = executor.invokeAll(tasks, 5, TimeUnit.SECONDS);
+
+        // 等待任务全部结束
+        await().atMost(5, TimeUnit.SECONDS).until(() -> resultCount.get() == 20);
+
+        // 确认所有的任务都执行完毕
+        then(futures).allMatch(Future::isDone);
+
+        // 确认任务计算结果
+        then(futures).map(Future<Integer>::get).containsExactly(1, 2, 3, 5, 8, 13, 21,
+            34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946);
+    }
+
+    /**
+     * 批量提交多个任务
+     *
+     * <p>
+     * 通过 {@link ExecutorService#invokeAny(java.util.Collection, long, TimeUnit)
+     * ExecutorService.invokeAny(Collection, long, TimeUnit)} 方法可以批量提交多个任务, 且返回第一个执行完毕的任务结果
+     * </p>
+     *
+     * <p>
+     * 在多个提交的任务中, 一旦有一个成功执行完毕, 则其它的任务均被取消, 例如在多个数据表中查找一条数据, 一旦一个任务成功完成,
+     * 则其它任务就无需继续执行
+     * </p>
+     */
+    @Test
+    void invokeAny_shouldSubmitMultiTasksAndInvokeAnyOfThem() throws Exception {
+        var rand = new Random();
+
+        // 保存 FutureTask 的集合对象
+        var tasks = IntStream.range(1, 21)
+                .mapToObj(n -> (Callable<String>) () -> {
+                    var sleepMillis = rand.nextInt(2000) + 1000;
+                    Thread.sleep(sleepMillis);
+                    return String.format("%d-Success-Sleep-%d", n, sleepMillis);
+                })
+                .toList();
+
+        // 创建线程池执行器对象
+        var executor = arrayBlockingQueueExecutor(20);
+        // 执行所有任务
+        var result = executor.invokeAny(tasks, 5, TimeUnit.SECONDS);
+
+        // 确认任务计算结果
+        then(result).matches("^[1-2]?\\d-Success-Sleep-1\\d{3}$");
     }
 }
