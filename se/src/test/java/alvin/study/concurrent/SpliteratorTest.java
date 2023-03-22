ab123a23e1f4386a5e0049606ae30073fe9877e8
AlvinQ <hqu@hipacloud.com>
2023-03-22 23:06:56 +0800

    [se] add concurrent demo

--- se/src/test/java/alvin/study/concurrent/SpliteratorTest.java
+++ se/src/test/java/alvin/study/concurrent/SpliteratorTest.java
@@ -2,7 +2,11 @@ package alvin.study.concurrent;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Spliterator;
 import java.util.stream.IntStream;
+import java.util.stream.StreamSupport;
 
 import org.junit.jupiter.api.Test;
 
@@ -12,16 +16,25 @@ import org.junit.jupiter.api.Test;
 @SuppressWarnings("java:S2925")
 class SpliteratorTest {
     @Test
-    void split_should() throws InterruptedException {
+    void trySplit_shouldSplitCollectionIntoSlice() throws InterruptedException {
         var list = IntStream.range(0, 10).boxed().toList();
 
         var part1 = list.stream().spliterator();
         then(part1.estimateSize()).isEqualTo(10);
 
         var part2 = part1.trySplit();
-        then(part2.estimateSize()).isEqualTo(part1.estimateSize()).isEqualTo(5);
+        then(part1.estimateSize()).isEqualTo(5);
+        then(part2.estimateSize()).isEqualTo(5);
 
-        part1.tryAdvance(n -> then(n).isIn(1, 2, 3, 4, 5));
-        part2.tryAdvance(n -> then(n).isIn(6, 7, 8, 9, 0));
+        then(toList(part1, part2)).containsExactlyInAnyOrder(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+    }
+
+    @SafeVarargs
+    private static <T> List<T> toList(Spliterator<T>... spliterators) {
+        var result = new ArrayList<T>();
+        for (var sp : spliterators) {
+            StreamSupport.stream(sp, false).forEach(result::add);
+        }
+        return result;
     }
 }
