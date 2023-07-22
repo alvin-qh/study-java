24706ccdf804394f20f9acbc684f5198ed40d814
Alvin <quhao317@163.com>
2023-07-22 23:09:23 +0800

    Restructure the code

---
+++ springboot/jooq/src/main/java/alvin/study/util/boot/SpringBeanUtil.java
@@ -0,0 +1,63 @@
+package alvin.study.util.boot;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * 辅助获取 Bean 对象的工具类
+ */
+@Component
+@SuppressWarnings("unused")
+public class SpringBeanUtil implements ApplicationContextAware {
+    // 应用上下文对象
+    private static ApplicationContext context;
+
+    /**
+     * 注入 {@link ApplicationContext} 对象
+     */
+    @Override
+    public void setApplicationContext(@NotNull ApplicationContext applicationContext) throws BeansException {
+        if (context == null) {
+            context = applicationContext;
+        }
+    }
+
+    /**
+     * 获取 {@link ApplicationContext} 对象
+     */
+    public static ApplicationContext getApplicationContext() {return context;}
+
+    /**
+     * 通过名称获取 Bean
+     *
+     * @param name Bean 名称
+     * @return Bean 对象
+     */
+    public static Object getBean(String name) {
+        return context.getBean(name);
+    }
+
+    /**
+     * 通过类型获取 Bean
+     *
+     * @param clazz Bean 类型
+     * @return Bean 对象
+     */
+    public static <T> T getBean(Class<T> clazz) {
+        return context.getBean(clazz);
+    }
+
+    /**
+     * 通过名称和类型获取 Bean
+     *
+     * @param name  Bean 名称
+     * @param clazz Bean 类型
+     * @return Bean 对象
+     */
+    public static <T> T getBean(String name, Class<T> clazz) {
+        return context.getBean(name, clazz);
+    }
+}
---
+++ springboot/jooq/src/test/java/alvin/study/IntegrationTest.java
@@ -0,0 +1,101 @@
+package alvin.study;
+
+import alvin.study.core.context.Context;
+import alvin.study.core.context.CustomRequestAttributes;
+import alvin.study.core.context.WebContext;
+import alvin.study.core.jooq.dsl.JdbcDSLContextManager;
+import alvin.study.infra.model.UserType;
+import alvin.study.testing.conf.TestConfig;
+import alvin.study.testing.conf.TestContextInitializer;
+import alvin.study.testing.data.TableCleaner;
+import alvin.study.util.security.PasswordUtil;
+import org.jooq.DSLContext;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static alvin.study.infra.model.public_.tables.Org.ORG;
+import static alvin.study.infra.model.public_.tables.User.USER;
+
+/**
+ * 集成测试超类, 所有需要数据库集成测试, 需要继承此类
+ */
+@ActiveProfiles("test")
+@SpringBootTest(classes = {TestConfig.class})
+@ContextConfiguration(initializers = {TestContextInitializer.class})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public abstract class IntegrationTest {
+    // 用于产生不重复名称的原子整数对象
+    protected static final AtomicInteger SEQUENCE = new AtomicInteger();
+
+    // 注入数据表清理器对象
+    @Autowired
+    private TableCleaner tableCleaner;
+
+    // DSLContext 管理器
+    @Autowired
+    protected JdbcDSLContextManager contextManager;
+
+    /**
+     * Jooq DSL Context 对象
+     */
+    @Autowired
+    protected DSLContext dsl;
+
+    /**
+     * 密码加密计算工具对象
+     */
+    @Autowired
+    protected PasswordUtil passwordUtil;
+
+    /**
+     * 每次测试执行前执行
+     */
+    @BeforeEach
+    void beforeEach() {
+        // 测试开始前, 清理数据表
+        tableCleaner.clearAllTables("schema_version", "flyway_schema_history");
+
+        // 注册请求上下文对象
+        var context = CustomRequestAttributes.register(new WebContext());
+
+        // 构建 ORG, USER 对象存入上下文
+        dsl.transaction(c -> {
+            var org = dsl.newRecord(ORG);
+            org.setName(makeUniqueName("alvin.edu")).store();
+            context.set(Context.ORG, org);
+
+            var user = dsl.newRecord(USER);
+            user.setAccount("admin")
+                .setPassword(passwordUtil.encrypt("12345678"))
+                .setType(UserType.ADMIN)
+                .store();
+            context.set(Context.USER, user);
+        });
+    }
+
+    /**
+     * 每次测试完成后执行
+     */
+    @AfterEach
+    void afterEach() {
+        // 清理 DSLContext 和数据库连接
+        contextManager.clear();
+    }
+
+    /**
+     * 产生一个不重复的名称
+     *
+     * @param name 原名称
+     * @return 不重复的名称
+     */
+    protected String makeUniqueName(String name) {
+        return name + ":" + SEQUENCE.incrementAndGet();
+    }
+}
