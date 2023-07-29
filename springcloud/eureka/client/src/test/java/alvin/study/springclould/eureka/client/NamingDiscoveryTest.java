0deffc6cc1d440d32a780f607132dd027097f269
Alvin <quhao317@163.com>
2023-07-30 01:02:51 +0800

    clean code

---
+++ springcloud/eureka/client/src/test/java/alvin/study/springclould/eureka/client/NamingDiscoveryTest.java
@@ -0,0 +1,66 @@
+package alvin.study.springclould.eureka.client;
+
+import alvin.study.springclould.eureka.client.endpoint.model.HelloDto;
+import alvin.study.springclould.eureka.client.conf.EurekaClientConfig;
+import alvin.study.springclould.eureka.client.core.model.ResponseWrapper;
+import alvin.study.springclould.eureka.client.service.HelloService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+/**
+ * 集成测试类的超类
+ *
+ * <p>
+ * 集成测试指的是将数据库操作和业务操作集成在一起进行测试, 可以比较真实的复现业务执行的流程
+ * </p>
+ *
+ * <p>
+ * {@link ActiveProfiles @ActiveProfiles} 注解用于指定活动配置名, 通过指定为 {@code "test"},
+ * 令所有注解为 {@link org.springframework.context.annotation.Profile @Profile}
+ * 的配置类生效, 且配置文件 {@code classpath:/application-test.yml} 生效
+ * </p>
+ *
+ * <p>
+ * {@link SpringBootTest @SpringBootTest} 注解表示这是一个 Spring Boot 相关的测试
+ * </p>
+ */
+@ActiveProfiles("test")
+@SpringBootTest(classes = EurekaClientConfig.class)
+public class NamingDiscoveryTest {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private HelloService helloService;
+
+    @Test
+    void get_shouldVisitServiceByServiceName() throws InterruptedException {
+        var resp = restTemplate.getForObject("http://eureka-client/api/hello", ResponseWrapper.class);
+        then(resp)
+                .isNotNull()
+                .extracting(ResponseWrapper::getRetCode)
+                .isEqualTo(0);
+
+        var dto = objectMapper.convertValue(resp.getPayload(), HelloDto.class);
+        then(dto.getApplicationName()).isEqualTo("eureka-client");
+        then(dto.getContent()).isEqualTo("Hello Eureka");
+    }
+
+    @Test
+    void get_shouldVisitByHelloService() {
+        var resp = helloService.get();
+
+        then(resp.getRetCode()).isZero();
+        then(resp.getPayload().getApplicationName()).isEqualTo("eureka-client");
+        then(resp.getPayload().getContent()).isEqualTo("Hello Eureka");
+    }
+}
