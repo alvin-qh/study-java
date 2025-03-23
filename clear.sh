3b1ad3e355b30b49e1d56885ecf1e8be69ecb85a
Alvin <quhao317@163.com>
2025-03-24 01:54:37 +0800

    update clean.sh script

--- clear.sh
+++ clear.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
+set -ex
+
 find . -name target | xargs rm -rf
-
 find . -name .data | xargs rm -rf
-
 find . -name build | xargs rm -rf
-
 find . -name *.log | xargs rm -f
 
 rm -rf $HOME/logs $HOME/nacos
