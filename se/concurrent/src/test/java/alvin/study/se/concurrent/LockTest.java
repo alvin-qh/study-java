a4da20e48204ebeef6f73ac758f9bc968584c3b1
AlvinQ <quhao317@163.com>
2025-03-28 19:57:36 +0800

    [se] update demo

--- se/concurrent/src/test/java/alvin/study/se/concurrent/LockTest.java
+++ se/concurrent/src/test/java/alvin/study/se/concurrent/LockTest.java
@@ -3,8 +3,8 @@ package alvin.study.se.concurrent;
 import static org.assertj.core.api.BDDAssertions.then;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 import lombok.SneakyThrows;
@@ -12,16 +12,16 @@ import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 
 /**
- * 测试 {@link Lock} 接口
+ * 测试 {@link java.util.concurrent.locks.Lock Lock} 接口
  *
  * <p>
- * {@link Lock} 接口是 Java 并发编程中用于控制线程同步的接口,
- * 该接口定义了一个互斥锁
+ * {@link java.util.concurrent.locks.Lock Lock} 接口是 Java
+ * 并发编程中用于控制线程同步的接口, 该接口定义了一个互斥锁
  * </p>
  *
  * <p>
- * {@link ReentrantLock} 是 {@link Lock} 接口的一个实现类,
- * 实现了一个可重入锁, 即同一线程可以多次获取锁
+ * {@link ReentrantLock} 是 {@link java.util.concurrent.locks.Lock
+ * Lock} 接口的一个实现类, 实现了一个可重入锁, 即同一线程可以多次获取锁
  * </p>
  *
  * <p>
@@ -38,21 +38,66 @@ import org.junit.jupiter.api.Test;
 public class LockTest {
     @Test
     @SneakyThrows
-    public void sss1() {
+    public void lock_shouldLockThreadByFairLock() {
+        var results = new LinkedHashMap<Integer, Integer>();
+
+        // 定义一个公平的可重入锁
+        var lock = new ReentrantLock(true);
+
+        // 启动 10 个线程, 同时访问 `result` 对象资源
+        var threads = new Thread[10];
+        for (var i = 0; i < threads.length; i++) {
+            var index = i + 1;
+
+            // 在线程内启动一个任务, 该任务会获取锁, 并将结果添加到 `results` 中
+            threads[i] = new Thread(() -> {
+                for (var n = 0; n < 100; n++) {
+                    // 由于 `lock` 对象表示一个公平锁, 所以会按照进入等待的顺序依次进入锁
+                    lock.lock();
+                    try {
+                        // 进入锁后, 执行代码
+                        results.compute(index, (key, val) -> val == null ? 1 : val + 1);
+                    } finally {
+                        // 代码执行完毕后, 解除锁定
+                        lock.unlock();
+                    }
+                }
+            });
+            threads[i].start();
+        }
+
+        for (var thread : threads) {
+            thread.join();
+        }
+
+        threads = null;
+    }
+
+    @Test
+    @SneakyThrows
+    public void lock_shouldLockThreadByUnfairLock() {
         var results = new ArrayList<Integer>();
 
         // 定义一个公平的可重入锁
-        Lock lock = new ReentrantLock(true);
+        var lock = new ReentrantLock(false);
 
+        // 启动 10 个线程, 同时访问 `result` 对象资源
         var threads = new Thread[10];
         for (var i = 0; i < threads.length; i++) {
             var index = i + 1;
 
             threads[i] = new Thread(() -> {
+                // 由于 `lock` 对象表示一个非公平锁, 所以会随机一个个线程先获取锁
                 lock.lock();
                 try {
+                    // 进入锁后, 执行代码
                     results.add(index);
                 } finally {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {}
+
+                    // 代码执行完毕后, 解除锁定
                     lock.unlock();
                 }
             });
@@ -70,7 +115,7 @@ public class LockTest {
     @SneakyThrows
     public void sss2() {
         // 定义一个公平的可重入锁
-        Lock lock = new ReentrantLock(true);
+        var lock = new ReentrantLock(true);
 
         lock.lock();
 
