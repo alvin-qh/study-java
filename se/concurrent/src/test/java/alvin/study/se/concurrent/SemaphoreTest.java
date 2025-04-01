4966f8dcb03cc856653c7ebc81b1c8a89e05e4d2
Alvin <quhao317@163.com>
2025-04-02 01:18:13 +0800

    [se/concurrent] add thread demo

--- se/concurrent/src/test/java/alvin/study/se/concurrent/SemaphoreTest.java
+++ se/concurrent/src/test/java/alvin/study/se/concurrent/SemaphoreTest.java
@@ -38,21 +38,31 @@ public class SemaphoreTest {
         // 创建具备 2 个许可证的信号量对象
         var semaphore = new Semaphore(MAX_THREADS / 2);
 
-        // 创建 4 个线程
+        // 创建 4 个线程对象
         var threads = new Thread[MAX_THREADS];
 
-        
+        // 记录线程执行结果
         var results = new ArrayList<Long>();
 
+        // 同时启动 4 个线程, 在每个线程中通过信号量获取许可证, 并在成功获取到许可证后,
+        // 记录获取到许可证的时间
+        // 在获取到许可证后, 线程休眠 100ms, 释放获取到的许可证
+        // 由于 `Semaphore` 的许可证数量为 2, 所以每次只能有两个线程可以同时获取到许可证,
+        // 结果就是: 在记录的结果中, 前两个线程结果的时间几乎一致, 后两个几乎一致,
+        // 第三个和第四个记录结果相差 100ms 左右
         for (int i = 0; i < threads.length; i++) {
             threads[i] = new Thread(() -> {
                 try {
+                    // 获取信号量许可证, 如果信号量暂时无许可证, 则阻塞当前线程
                     semaphore.acquire();
                     try {
+                        // 获取到许可证后, 记录获取到许可证的时间
                         synchronized (results) {
                             results.add(System.currentTimeMillis());
                         }
 
+                        // 休眠 100ms 后, 释放获取到的许可证, 故后续再要获取许可证,
+                        // 至少要在 100ms 之后
                         Thread.sleep(100);
                     } finally {
                         semaphore.release();
@@ -65,9 +75,11 @@ public class SemaphoreTest {
 
         Threads.joinAll(threads, 1000);
 
+        // 确认结果的前两项和后两项的时间差值, 应该在 0ms 左右
         then(results.get(1) - results.get(0)).isBetween(0L, 10L);
         then(results.get(3) - results.get(2)).isBetween(0L, 10L);
 
+        // 确认结果的第三项和第四项的时间差值, 应该在 100ms 左右
         then(results.get(2) - results.get(1)).isBetween(100L, 110L);
     }
 }
